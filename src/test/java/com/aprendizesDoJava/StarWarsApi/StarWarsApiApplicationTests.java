d04df1bd43ed38208096f99abca78725877c2dbd
Victor-Ferreira <victoraugusto103587@gmail.com>
2025-06-18 00:05:28 -0300

    build(JAVAINIT-6): Configurar ambiente de desenvolvimento - Victor
    Settado Spring e dependencias

---
+++ src/test/java/com/aprendizesDoJava/StarWarsApi/StarWarsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aprendizesDoJava.StarWarsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
