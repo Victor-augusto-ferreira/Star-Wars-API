23dd420a53917958a2290d3e565343f881ca0949
Victor-Ferreira <victoraugusto103587@gmail.com>
2025-06-21 01:37:41 -0300

    feat(JAVAINIT-10): Adicionado endpoint 'Hello World'

---
+++ src/main/java/com/aprendizesDoJava/StarWarsApi/controller/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.aprendizesDoJava.StarWarsApi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/hello-world")
+public class HelloWorldController {
+
+    @GetMapping
+    public ResponseEntity<String> getHelloWorld() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
