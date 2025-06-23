985f1a0d15f9b2f858235e8361f0bd3fa0f5f541
Ricardo <ricardo.junior.junior@hotmail.com>
2025-06-22 21:04:28 -0300

    JAVAINIT-13: implementa endpoint /api/v1/hello-world

---
+++ src/main/java/com/aprendizesDoJava/StarWarsApi/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.aprendizesDoJava.StarWarsApi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class HelloWorldController {
+
+    @GetMapping("/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
